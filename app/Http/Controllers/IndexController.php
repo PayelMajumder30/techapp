da576a46b5bca3656c680ec0ae73b72882a0714e
“snigdhadeep-php15” <snigdhadeep@codelogicx.com>
2025-02-11 08:29:34 +0530

    save

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,8 +9,18 @@ use App\Models\AdmissionForm;
 class IndexController extends Controller
 {
     //
-   public function formView(){
-    $admissionform = AdmissionForm::simplePaginate(2);
-    return view('admission.index', compact('admissionform'));
-   }
+    public function formView(Request $request){
+        $query = AdmissionForm::query();
+        $query->when(request('start_date') != '', function ($q) {
+            return $q->where('created_at', '>=', date('Y-m-d', strtotime(request('start_date'))));
+        });
+        $query->when(request('end_date') != '', function ($q) {
+            return $q->where('created_at', '<=', date('Y-m-d', strtotime(request('end_date'))));
+        });
+        $query->when(trim(request('keyword')) != '', function ($q) {
+            return $q->where('name', 'like', request('keyword') . '%');
+        });
+        $admissionform = $query->simplePaginate(4);
+        return view('admission.index', compact('admissionform'));
+    }
 }
