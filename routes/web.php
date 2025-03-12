9d293475ee79f8bd4edf0c7c4de5fe791ac9b091
“snigdhadeep-php15” <snigdhadeep@codelogicx.com>
2025-03-12 15:38:05 +0530

    Admin model is added

--- routes/web.php
+++ routes/web.php
@@ -57,14 +57,14 @@ Route::prefix('contact')->name('contact.')->group(function(){
 // Route::middleware(['auth'])->group(function () {
 //     Route::get('/school', [SchoolController::class, 'index'])->name('school');
 // });
-Route::controller(LoginRegisterController::class)->group(function() {
-    Route::get('/register', 'register')->name('register');
-    Route::post('/store', 'store')->name('store');
-    Route::get('/login', 'login')->name('login');
-    Route::post('/authenticate', 'authenticate')->name('authenticate');
-    Route::get('/dashboard', 'dashboard')->name('dashboard');
-    Route::post('/logout', 'logout')->name('logout');
-});
+// Route::controller(LoginRegisterController::class)->group(function() {
+//     Route::get('/register', 'register')->name('register');
+//     Route::post('/store', 'store')->name('store');
+//     Route::get('/login', 'login')->name('login');
+//     Route::post('/authenticate', 'authenticate')->name('authenticate');
+//     Route::get('/dashboard', 'dashboard')->name('dashboard');
+//     Route::post('/logout', 'logout')->name('logout');
+// });
 
 
 
